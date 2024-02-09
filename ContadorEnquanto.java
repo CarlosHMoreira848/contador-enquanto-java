4bc8d0d1c68ea91a951b81f4dcd5a9b2562dbb10
Carlos Henrique Moreira <84051885+CarlosHMoreira848@users.noreply.github.com>
2024-02-09 14:37:57 -0300

    Add files via upload

---
+++ ContadorEnquanto.java
@@ -0,0 +1,10 @@
+public class ContadorEnquanto {
+	public static void main(String[] args) {
+		int contador = 0;
+		System.out.println("Bem vindo ao programa! Testando...");
+		while(contador <= 10) {
+			System.out.println(contador);
+			contador ++;
+		}
+	}
+}
